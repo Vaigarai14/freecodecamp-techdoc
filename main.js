a17496f871bb9f6704eec55c06d9b9ab3804a824
Vaigarai14 <vaigaraithennarasu144@gmail.com>
2023-12-23 17:38:09 +0530

    attached js file to loop the hr tag to provide lines between the nav bar

---
+++ main.js
@@ -0,0 +1,13 @@
+var nav = document.getElementById("navbar");
+var h3 = document.getElementsByTagName("h3");
+var header = document.getElementById('title');
+
+function gethr() {
+    for (var i = 0; i < h3.length; i++) {
+        var k = h3[i]
+        var hr = document.createElement('hr');
+        k.appendChild(hr);
+    }
+}
+gethr()
+
